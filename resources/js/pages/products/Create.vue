ff64dbb383e81fc82b03cfbe437abdb3bf294091
Jan Noel S. Paed <Jannoelpaed27@gmail.com>
2025-11-21 23:09:57 +0800

    added form

--- resources/js/pages/products/Create.vue
+++ resources/js/pages/products/Create.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import Input from '@/components/ui/input/Input.vue';
+import Label from '@/components/ui/label/Label.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { productsCreate } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
@@ -16,6 +18,17 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Create a Product" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="p-4">Create a Product Page</div>
+        <div class="p-4">
+            <form action="" class="mt-4 w-8/12 space-y-4">
+                <div class="space-y-2">
+                    <Label for="Product Name">Name</Label>
+                    <Input type="text" placeholder="Name" />
+                </div>
+                <div class="space-y-2">
+                    <Label for="Product Name">Name</Label>
+                    <Input type="text" placeholder="Name" />
+                </div>
+            </form>
+        </div>
     </AppLayout>
 </template>
